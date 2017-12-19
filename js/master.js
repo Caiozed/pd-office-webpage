68c19123b1e18ebe65ec7c65b888f75cbd05cb8e
Caio Neves <caio.nevesleite@gmail.com>
2017-12-19 22:20:22 +0000

    Project setup

---
+++ js/master.js
@@ -0,0 +1,21 @@
+var app = angular.module("escritorio", ["ngRoute"]);
+app.config(function($routeProvider){
+    $routeProvider.
+    when("/", {
+        templateUrl: "templates/home.html"
+    }).
+    when("/clientes", {
+        templateUrl: "templates/clients.html"
+    }).
+    when("/sobre", {
+        templateUrl: "templates/about.html"
+    }).
+    when("/contato", {
+        templateUrl: "templates/contact.html"
+    });
+});
+
+
+$(document).on("click", ".dropdown-toggler", function(){
+    $(this).parent("section").children(".dropdown").slideToggle("slow");
+})
